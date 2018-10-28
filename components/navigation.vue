8066b2455043b44d60cca3b4ba796d711646170a
Sonia <sonia@matise.nl>
2018-10-28 14:55:17 +0100

    all the links

--- components/navigation.vue
+++ components/navigation.vue
@@ -1,16 +1,16 @@
 <template>
 	<header class="header">
 		<nuxt-link to="/">
-			<div class="logo column" :style="``"></div>
+			<div class="logo column" :style="``">Sepiroth</div>
 		</nuxt-link>
 		
 		<nav class="nav top-nav">
-			<ul class="menu__list" v-for="item in items" :key="item.id">
+			<ul class="menu__list" v-for="item in menuitems" :key="item.id">
 				<li class="menu__item">
 					<nuxt-link
-						:to="item.slug"
+						:to="item.object_slug"
 						class="menu__link">
-						{{ item.title.rendered }}
+						{{ item.title }}
 					</nuxt-link>
 				</li>
 			</ul>
@@ -25,18 +25,15 @@ const apiDomain = process.env.apiDomain;
 export default {
 	data(){
 		return {
-			items: [],
+			menuitems: [],
 		}
-	},
-	components: {
-		
 	},
 	mounted(){
 		let self = this;
-		axios.get(apiDomain + '/wp/v2/pages').then(result => {
-			let pagesAll = result.data;
-			pagesAll.forEach(element => {
-				self.items.push(element);
+		axios.get(apiDomain + '/wp-api-menus/v2/menus/2').then(result => {
+			let menuAll = result.data.items;
+			menuAll.forEach(element => {
+				self.menuitems.push(element);
 			});
 		});
 	}
